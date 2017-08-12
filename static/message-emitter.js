74858b7856cc898d7803ce94eec59d3e57f6a0b7
Denis Rykovanov <denis.rykovanov@mail.ru>
2017-08-12 08:05:15 +0300

    #1 clean up before actual implementation emitter was extracted

---
+++ static/message-emitter.js
@@ -0,0 +1,49 @@
+/**
+ * Created by drykovanov on 12.08.2017.
+ */
+
+function getMessageEmitter(socket) {
+
+    socket.on('message', onMessage);
+
+    function onMessage(msg) {
+        console.log('on message', msg);
+    }
+
+    function emitMsg(data) {
+        socket.emit('message', data)
+    }
+
+    function emitCmd(cmd, parameters) {
+        emitMsg({ cmd: cmd, parameters: parameters || {} })
+    }
+
+    function msgForward() {
+        emitCmd('direction:FORWARD;');
+    }
+
+    function msgLeft() {
+        emitCmd('dir-left:;');
+    }
+
+    function msgRight() {
+        emitCmd('dir-right:;')
+    }
+
+    function msgBack() {
+        emitCmd('direction:BACKWARD;');
+    }
+
+    function msgRelease() {
+        emitCmd('direction:RELEASE;');
+    }
+
+    return {
+        emitCmd: emitCmd,
+        msgForward: msgForward,
+        msgBack: msgBack,
+        msgLeft: msgLeft,
+        msgRight: msgRight,
+        msgRelease: msgRelease
+    }
+}
