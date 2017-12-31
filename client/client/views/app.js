ae26ae6e3e429349e19072739f96990af048a3b2
Denis Rykovanov <denis.rykovanov@mail.ru>
2017-12-31 09:25:51 +0300

    added gamepad joystik support

--- client/client/views/app.js
+++ client/client/views/app.js
@@ -1,6 +1,7 @@
 import { render } from 'react-dom';
 import CameraJoystik from './camera-joystick';
 import PlatformJoystik from './platform-joystick';
+import GamepadJoystik from './gamepad-joystick';
 import NippleJoystik from './nipple-joystick';
 import SocketStatus from './socket-status';
 import VideoPanel from './video-panel';
@@ -13,6 +14,7 @@ const App = (props) => {
     <CameraJoystik />
     <PlatformJoystik />
     <NippleJoystik />
+    <GamepadJoystik />
     <VideoPanel/>
     <Room/>
   </div>;
