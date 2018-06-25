67529fa808f8d2ecd73ba5e5331696d3b588acf4
Denis <denis.rykovanov@mail.ru>
2018-06-26 00:03:22 +0300

    #8 cosmetic corrections

--- start.sh
+++ start.sh
@@ -10,8 +10,12 @@
 #
 ### END INIT INFO
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
 
-export basedir=`dirname $0`
+export basedir=SCRIPTPATH
 export port=443
 
 if [[ $env = "dev" ]]
