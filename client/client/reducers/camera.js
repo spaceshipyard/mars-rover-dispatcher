c6eba4ea064ed4790769ea84601ca966e36b573d
Denis <denis.rykovanov@mail.ru>
2019-02-27 19:54:09 +0300

    #40 small cosmetics changes during review

--- client/client/reducers/camera.js
+++ client/client/reducers/camera.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 
-const cameraReducer = (state = { x: 0, y: 0 }, action) => {
+const INITIAL_STATE = { x: 90, y: 90 }
+
+const cameraReducer = (state = INITIAL_STATE, action) => {
   if (action.type === 'camUpdate') {
-    state = action.value
+    state = { ...state, ...action.value }
   }
   return state
 }
--- client/client/views/camera-joystick.js
+++ client/client/views/camera-joystick.js
@@ -1,48 +1,42 @@
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
-const data = { x: 90, y: 90, min: 0, max: 180 }
+const MIN = 0
+const MAX = 180
+const STEP = 5
 
-const CameraJoystik = ({ onChange, x, y }) => {
-  const getValue = (event) => event.target.value
+const incByStepUtilMax = (val) => Math.min(val + STEP, MAX)
+const decByStepUntilMin = (val) => Math.max(val - STEP, MIN)
+const eventToValue = (event) => +event.target.value
+
+const CameraJoystik = ({ onChange, currOffset }) => {
+  const { x, y } = currOffset
+  const onPartialInput = (newPartialOffset) => {
+    const newOffset = { ...currOffset, ...newPartialOffset }
+    !_.isEqual(currOffset, newOffset) && onChange(newOffset)
+  }
+
   return <div>
     <span>Camera Joystick</span>
     <div>
       <div className='camera-controls'>
-        <button onClick={() => {
-          if (data.x > data.min) {
-            data.x -= 5
-            onChange({ x: data.x })
-          }
-        }}>⬅</button>
-        <input type='range' min={data.min} max={data.max} step='5' value={x} onChange={(e) => onChange({ y, x: getValue(e) })} />
-        <button onClick={() => {
-          if (data.x < data.max) {
-            data.x += 5
-            onChange({ x: data.x })
-          }
-        }}>➡</button>
+        <button onClick={() => onPartialInput({ x: decByStepUntilMin(x) })}>⬅</button>
+        <input type='range' min={MIN} max={MAX} step={STEP} value={x}
+          onChange={(e) => onPartialInput({ x: eventToValue(e) })} />
+        <button onClick={() => onPartialInput({ x: incByStepUtilMax(x) })}>➡</button>
       </div>
       <div className='camera-controls'>
-        <button onClick={() => {
-          if (data.y > data.min) {
-            data.y -= 5
-            onChange({ y: data.y })
-          }
-        }}>⬅</button>
-        <input type='range' min={data.min} max={data.max} step='5' value={y} onChange={(e) => onChange({ x, y: getValue(e) })} />
-        <button onClick={() => {
-          if (data.y < data.max) {
-            data.y += 5
-            onChange({ y: data.y })
-          }
-        }}>➡</button>
+        <button onClick={() => onPartialInput({ y: decByStepUntilMin(y) })}>⬅</button>
+        <input type='range' min={MIN} max={MAX} step={STEP} value={y}
+          onChange={(e) => onPartialInput({ y: eventToValue(e) })} />
+        <button onClick={() => onPartialInput({ y: incByStepUtilMax(y) })}>➡</button>
       </div>
     </div>
   </div>
 }
 
 const connectToCammera = connect(
-  ({ camera }) => ({ x: camera.x, y: camera.y }),
+  ({ camera: { offset: { x, y } } }) => ({ currOffset: { x, y } }),
   (dispatch) => ({ onChange: ({ x, y }) => dispatch({ type: 'camUpdate', value: { x, y } }) }))
 
 export default connectToCammera(CameraJoystik)
