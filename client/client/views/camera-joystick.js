c576c30e71c887f273b72872380bde33b9140287
chaos-adept <denis.rykovanov@mail.ru>
2019-02-28 22:19:08 +0300

    #40 naming, typo fixed

--- client/client/views/camera-joystick.js
+++ client/client/views/camera-joystick.js
@@ -5,15 +5,15 @@ const MIN = 0
 const MAX = 180
 const STEP = 5
 
-const incByStepUtilMax = (val) => Math.min(val + STEP, MAX)
+const incByStepUntilMax = (val) => Math.min(val + STEP, MAX)
 const decByStepUntilMin = (val) => Math.max(val - STEP, MIN)
 const eventToValue = (event) => +event.target.value
 
-const CameraJoystik = ({ onChange, currOffset }) => {
-  const { x, y } = currOffset
+const CameraJoystik = ({ onChange, offset }) => {
+  const { x, y } = offset
   const onPartialInput = (newPartialOffset) => {
-    const newOffset = { ...currOffset, ...newPartialOffset }
-    !_.isEqual(currOffset, newOffset) && onChange(newOffset)
+    const newOffset = { ...offset, ...newPartialOffset }
+    !_.isEqual(offset, newOffset) && onChange(newOffset)
   }
 
   return <div>
@@ -23,20 +23,20 @@ const CameraJoystik = ({ onChange, currOffset }) => {
         <button onClick={() => onPartialInput({ x: decByStepUntilMin(x) })}>⬅</button>
         <input type='range' min={MIN} max={MAX} step={STEP} value={x}
           onChange={(e) => onPartialInput({ x: eventToValue(e) })} />
-        <button onClick={() => onPartialInput({ x: incByStepUtilMax(x) })}>➡</button>
+        <button onClick={() => onPartialInput({ x: incByStepUntilMax(x) })}>➡</button>
       </div>
       <div className='camera-controls'>
         <button onClick={() => onPartialInput({ y: decByStepUntilMin(y) })}>⬅</button>
         <input type='range' min={MIN} max={MAX} step={STEP} value={y}
           onChange={(e) => onPartialInput({ y: eventToValue(e) })} />
-        <button onClick={() => onPartialInput({ y: incByStepUtilMax(y) })}>➡</button>
+        <button onClick={() => onPartialInput({ y: incByStepUntilMax(y) })}>➡</button>
       </div>
     </div>
   </div>
 }
 
 const connectToCammera = connect(
-  ({ camera: { offset: { x, y } } }) => ({ currOffset: { x, y } }),
+  ({ camera: { offset } }) => ({ offset }),
   (dispatch) => ({ onChange: ({ x, y }) => dispatch({ type: 'camUpdate', value: { x, y } }) }))
 
 export default connectToCammera(CameraJoystik)
